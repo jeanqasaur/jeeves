3f18e3838bb83894277e883107ed3b321d95ff63
Jean Yang <jean.yang.writeme@gmail.com>
2013-10-02 15:02:37 -0400

    added some more comments about how we might want to do the password policy

--- demo/authentication/AuthConfidentiality.py
+++ demo/authentication/AuthConfidentiality.py
@@ -2,16 +2,17 @@
 Authentication demo example for Jeeves with confidentiality policies.
 '''
 import JeevesGlobal
-#import fast.AST
+from fast.AST import And
 
 class User:
   def __init__(self, userId, name, pwd):
     self.userId = userId
     self.name = name
     self.pwdLabel = JeevesGlobal.jeevesLib.mkLabel()
-    # TODO: Can we check equality like this? If we do it right, there should
-    # be a circular dependency on the password equality. (Is that right?)
-    JeevesGlobal.jeevesLib.restrict(self.pwdLabel, lambda oc: oc == self)
+    # TODO`: Figure out how deep this equality goes...
+    #JeevesGlobal.jeevesLib.restrict(self.pwdLabel
+    #    , lambda oc: And(oc.userId == self.userId
+    #         , And(oc.name == self.name, oc.getPwd() == self.getPwd())))
     self.pwd = JeevesGlobal.jeevesLib.mkSensitive(self.pwdLabel, pwd, "")
 
-  def getPwd(self): return self.pwd
+  def getPwd(): return self.pwd
