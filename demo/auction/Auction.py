1263adab9136302643aad2064908da289571349e
Jean Yang <jean.yang.writeme@gmail.com>
2013-10-02 15:15:19 -0400

    small fix to something that somehow got changed back in AuthConfidentiality.py

--- demo/auction/Auction.py
+++ demo/auction/Auction.py
@@ -4,7 +4,10 @@ Authentication demo example for Jeeves with confidentiality policies.
 from macropy.case_classes import macros, case
 import JeevesGlobal
 
+@case
 class User:
+  pass
+'''
   def __init__(self, userId, name, pwd):
     self.userId = userId
     self.name = name
@@ -16,3 +19,4 @@ class User:
     self.pwd = JeevesGlobal.jeevesLib.mkSensitive(self.pwdLabel, pwd, "")
 
   def getPwd(self): return self.pwd
+  '''
