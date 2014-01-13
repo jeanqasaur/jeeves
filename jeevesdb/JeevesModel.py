ce76ce2c004791861a818e0ee7ac697abec0f0f1
Jean Yang <jean.yang.writeme@gmail.com>
2014-01-13 15:46:20 -0500

    small updates to jeevesdb tests

--- jeevesdb/JeevesModel.py
+++ jeevesdb/JeevesModel.py
@@ -3,13 +3,20 @@ Questions:
 - Can we get what we need by subclassing Model?
 '''
 from django.db import models
+import JeevesAPI
 
 # Make a Jeeves Model that enhances the vanilla Django model with information
 # about how labels work and that kind of thing. We'll also need to override
 # some methods so that we can create records and make queries appropriately.
 class JeevesModel(models.Model):
-  # Can we implement a Jeeves model on top of the regular model without
-  # overloading everything? (Let's try!)
+  # One extra column with one label for now.
+  # Eventually, we'll need a list of labels to capture things we save. At that
+  # point, we'll also need to figure out a strategy for not making labels
+  # blow up.
+  labels = models.CharField(max_length=10)
+
+  class Meta:
+    abstract = True
 
   '''
   def __init__(self):
@@ -18,8 +25,3 @@ class JeevesModel(models.Model):
     # then manipulate it.
     self.jeeves = JeevesAPI(self.objects, self.filter, self.all)
   '''
-
-  # One extra column with one label for now.
-  # Eventually, we'll need a list of labels to capture things we save. At that
-  # point, we'll also need to figure out a strategy for not making labels
-  # blow up.
--- test/jeevesdb/testdb/models.py
+++ test/jeevesdb/testdb/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from jeevesdb import JeevesModel
 
-class Animal(models.Model):
+class Animal(JeevesModel.JeevesModel): #models.Model):
   # TODO: Allow this to be a JeevesModel.JeevesModel. Right now this doesn't
   # validate...
   name = models.CharField(max_length=30)
