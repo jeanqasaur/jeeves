0659adfc779f15454facfe5f829735fda802ad37
Jean Yang <jean.yang.writeme@gmail.com>
2014-01-14 14:18:24 -0500

    adding JeevesAPI.py

---
+++ jeevesdb/JeevesAPI.py
@@ -0,0 +1,29 @@
+class JeevesAPI:
+  def __init__(self, objects):
+    self.objects = objects
+
+  def create(**kwargs):
+    pass
+
+  def get(self, objectId):
+    pass
+
+  def save():
+    pass
+
+  def all():
+    pass
+
+  def filter():
+    pass
+
+  # By default, order by random IDs.
+  def order_by():
+    pass
+
+  # We shouldn't need to overload join as long as we have this multi-row
+  # approach for labels! Very cool!
+  '''
+  def join():
+    pass
+  '''
