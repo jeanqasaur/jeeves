22adbac5d6f24f0fefc3deb4796d49e6805c46c0
Jean Yang <jean.yang.writeme@gmail.com>
2014-03-13 22:12:28 -0400

    basic sanitization example without endorsement

---
+++ test/gallery/endorsement/testSanitization.py
@@ -0,0 +1,48 @@
+import JeevesLib
+from smt.Z3 import *
+import unittest
+from macropy.case_classes import macros, enum
+
+from fast.ProtectedRef import ProtectedRef, UpdateResult
+import JeevesLib
+from sourcetrans.macro_module import macros, jeeves
+
+class User:
+  def __init__(self, userId):
+    self.userId = userId
+
+# Test that once values are sanitized, they may flow to the output, but not
+# before.
+class TestSanitization(unittest.TestCase):
+  def setUp(self):
+    # Need to initialize the JeevesLib environment.
+    JeevesLib.init()
+
+    self.alice = User(0)
+    self.bob = User(1)
+
+  # If the input does not do anything bad to our data structure, then we
+  # allow it to pass.
+  @jeeves
+  def testBehavioralSanitization(self):
+    touchedBadData = False
+    def f(s):
+      global touchedBadData
+      if s == "bad":
+        touchedBadData = True
+      return s
+    x = ProtectedRef("dunno", None
+      , lambda _this: lambda ic: lambda oc: not touchedBadData)
+    self.assertEqual(JeevesLib.concretize(None, x.v), "dunno")
+    assert x.update(None, None, f("good")) == UpdateResult.Success
+    self.assertEqual(JeevesLib.concretize(None, x.v), "good")
+    assert x.update(None, None, f("bad")) == UpdateResult.Failure
+    self.assertEqual(JeevesLib.concretize(None, x.v), "good")
+
+  def testEndorseSanitizeFunction(self):
+    pass
+    # TODO: Need a way of associating sanitization function with some sort of
+    # endorsement, probably with facet rewriting.
+
+if __name__ == '__main__':
+    unittest.main()
