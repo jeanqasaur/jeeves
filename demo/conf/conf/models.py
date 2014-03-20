539c1fd2e7eb5723bea7a0ca8361f892462ccb8e
Travis Hance <tjhance7@gmail.com>
2014-03-20 12:52:25 -0400

    fixed some profile stuff

--- demo/conf/conf/models.py
+++ demo/conf/conf/models.py
@@ -114,3 +114,16 @@ class Comment(Model):
 
     class Meta:
         db_table = 'comments'
+
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created and instance.is_superuser: 
+        UserProfile.objects.create(
+            username=instance.username,
+            email=instance.email,
+            level='chair',
+        )
