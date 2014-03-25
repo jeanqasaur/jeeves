a24962aa4a8258c38d1e07fb9b99fa69f04d5866
Jean Yang <jean.yang.writeme@gmail.com>
2014-03-25 00:48:24 -0400

    putting withdraw code back

--- demo/conf/conf/views.py
+++ demo/conf/conf/views.py
@@ -107,7 +107,6 @@ def about_view(request):
 def papers_view(request):
     user = UserProfile.objects.get(username=request.user.username)
 
-    '''
     # If the user has withdrawn a paper, then we need to take care of that.
     if request.method == 'POST':
       withdraw = request.POST.get('withdraw', None)
@@ -117,7 +116,6 @@ def papers_view(request):
         print paper_id.v
         if paper_id != None:
           Paper.objects.filter(jeeves_id=paper_id, author=user).delete()
-    '''
 
     papers = Paper.objects.all()
     paper_data = JeevesLib.JList2()
