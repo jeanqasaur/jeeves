882d5a201fbd723d3b076e0474dddf1cceb89f9c
Jean Yang <jean.yang.writeme@gmail.com>
2014-03-25 00:42:33 -0400

    trying to revert withdraw bug

--- demo/conf/conf/views.py
+++ demo/conf/conf/views.py
@@ -107,6 +107,7 @@ def about_view(request):
 def papers_view(request):
     user = UserProfile.objects.get(username=request.user.username)
 
+    '''
     # If the user has withdrawn a paper, then we need to take care of that.
     if request.method == 'POST':
       withdraw = request.POST.get('withdraw', None)
@@ -116,6 +117,7 @@ def papers_view(request):
         print paper_id.v
         if paper_id != None:
           Paper.objects.filter(jeeves_id=paper_id, author=user).delete()
+    '''
 
     papers = Paper.objects.all()
     paper_data = JeevesLib.JList2()
