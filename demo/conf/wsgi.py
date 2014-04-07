04e8d01c7a45e4b39b358f32fbd09a356fc2b8e6
Jean Yang <jean.yang.writeme@gmail.com>
2014-04-07 15:18:59 -0400

    removed print from wsgi config

--- demo/conf/wsgi.py
+++ demo/conf/wsgi.py
@@ -15,9 +15,5 @@ sys.path.append(os.path.join(os.path.dirname(__file__), 'conf/'))
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../'))
 os.environ["DJANGO_SETTINGS_MODULE"] = "conf.settings"
 
-import os
-import sys
-print >>sys.stderr, sys.path
-
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
