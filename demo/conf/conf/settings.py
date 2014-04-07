3f28d905f1d603d7865981e1ec9e0e06df203eb2
Jean Yang <jean.yang.writeme@gmail.com>
2014-04-07 15:16:14 -0400

    fixed settings.py

--- demo/conf/conf/settings.py
+++ demo/conf/conf/settings.py
@@ -73,7 +73,7 @@ WSGI_APPLICATION = 'wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(PROJECT_DIR, 'jconf.db'),
+        'NAME': os.path.join(BASE_DIR, 'jconf.db'),
     }
 }
 
