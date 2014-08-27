f0326feda9d3d8a35e171ec4143865fb9a46bb52
Jean Yang <jean.yang.writeme@gmail.com>
2014-08-27 13:43:28 -0400

    adding relevant static files

--- demo/hipaa/jelf/models.py
+++ demo/hipaa/jelf/models.py
@@ -14,6 +14,7 @@ import JeevesLib
 import csv
 import os
 
+# TODO: Figure out what this is.
 file=open(os.path.join(settings.BASE_DIR, '2010ZipcodePopulation.csv'))
 reader = csv.reader(file)
 zipCodes = {}
