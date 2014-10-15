eafeb7ffe7188b3ceb4e402b83c74bebc0cffce7
Jean Yang <jean.yang.writeme@gmail.com>
2014-10-15 17:59:59 -0400

    funkloading

---
+++ test/web/conf/test_Conf.py
@@ -0,0 +1,28 @@
+# -*- coding: iso-8859-15 -*-
+"""Conf FunkLoad test
+
+$Id$
+"""
+import unittest
+from random import random
+from funkload.FunkLoadTestCase import FunkLoadTestCase
+
+class Conf(FunkLoadTestCase):
+    """This test use a configuration file Conf.conf."""
+
+    def setUp(self):
+        """Setting up test."""
+        self.server_url = self.conf_get('main', 'url')
+
+    def test_simple(self):
+        # The description should be set in the configuration file
+        server_url = self.server_url
+        # begin of test ---------------------------------------------
+        nb_time = self.conf_getInt('test_simple', 'nb_time')
+        for i in range(nb_time):
+            self.get(server_url, description='Get url')
+        # end of test -----------------------------------------------
+
+
+if __name__ in ('main', '__main__'):
+    unittest.main()
