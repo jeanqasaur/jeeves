7dffec94a0b17c088c5dbfc2880bc7ce7678b9a7
Jean Yang <jean.yang.writeme@gmail.com>
2014-10-17 21:25:39 -0400

    more funkloader tests

---
+++ test/web/hipaa/test_Hipaa.py
@@ -0,0 +1,51 @@
+# -*- coding: iso-8859-15 -*-
+"""Conf FunkLoad test
+
+$Id$
+"""
+import unittest
+from random import random
+from funkload.FunkLoadTestCase import FunkLoadTestCase
+
+class Hipaa(FunkLoadTestCase):
+    """This test use a configuration file Conf.conf."""
+
+    def setUp(self):
+        """Setting up test."""
+        self.server_url = self.conf_get('main', 'url')
+
+    def test_simple(self):
+        # The description should be set in the configuration file
+        server_url = self.server_url + "/about"
+        # begin of test ---------------------------------------------
+        nb_time = self.conf_getInt('test_simple', 'nb_time')
+        for i in range(nb_time):
+            self.get(server_url, description='Get url')
+        # end of test -----------------------------------------------
+
+    '''
+    def test_login(self):
+        # The description should be set in the configuration file
+        server_url = self.server_url
+
+        self.get(server_url + "/",
+            description="Get /")
+        reply = self.get(server_url + "/index",
+            description="Get index")
+
+        csrftoken = extract_token(self.getBody(), "name='csrfmiddlewaretoken' value='", "' />")
+        self.post(server_url + "/accounts/manager/login/?next=/index/",
+            ['csrfmiddlewaretoken', csrftoken],
+            ['redirect_to', '/company/config/dashboard/'],
+            ['id_username', 'jeanyang'],
+            ['id_password', 'hi'],
+            description="Post /accounts/login/")
+
+        self.assert_("login" not in self.getLastUrl(), "Error in login")
+
+        self.get(server_url + "/accounts/logout/",
+            description="Get /accounts/logout/")
+    '''
+
+if __name__ in ('main', '__main__'):
+    unittest.main()
