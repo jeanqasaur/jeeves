facc765de48bfaa4eceff29f06051a404850ef09
Jean Yang <jean.yang.writeme@gmail.com>
2015-06-25 11:01:16 -0400

    shared concretize env

--- demo/conf/conf/views.py
+++ demo/conf/conf/views.py
@@ -84,11 +84,10 @@ def request_wrapper(view_fn):
                 return HttpResponseRedirect(JeevesLib.concretize(profile, path))
 
             JeevesLib.set_viewer(profile)
-            concretizeState = JeevesLib.jeevesState.policyenv.getNewSolverState(profile)
+            concretizeState = JeevesLib.get_solverstate() #JeevesLib.jeevesState.policyenv.getNewSolverState(profile)
             def concretize(val):
                 return concretizeState.concretizeExp(val
                     , JeevesLib.jeevesState.pathenv.getEnv())
-            # concretize = lambda val : JeevesLib.concretize(profile, val)
             add_to_context(context_dict, request, template_name, profile, concretize)
 
             #print 'concretized is', concretize(context_dict['latest_title'])
